df69ac0d469276a84247b22d4cf430e8a57bf697
Dev093235 <dev431140@gmail.com>
2026-04-28 01:18:00 +0530

    Update radha.js

--- Rudra/commands/radha.js
+++ Rudra/commands/radha.js
@@ -15,7 +15,7 @@ module.exports.config = {
     cooldowns: 3,
 };
 
-const GROQ_API_KEY = "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";
+const GROQ_API_KEY = "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";
 const MODEL_NAME = "llama-3.3-70b-versatile";
 
 const BASE_DIR = path.join(__dirname, "temporary");
